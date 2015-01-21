1e0e2f8d37c46c4bbd931f2985c56756bbb6427f
Nathan Hutchison <nathan@bugherd.com>
2015-01-21 11:13:40 +1100

    add start bin to package.son

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ var plugins = [
   new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.js'),
 ];
 
-var jsxLoader = ['jsx'];
+var jsxLoader = ['jsx?harmony'];
 
 if (release)  {
   plugins.push(new webpack.DefinePlugin({
@@ -30,7 +30,7 @@ var config = module.exports = {
   devtool: !release && 'eval',
   entry: {
     app: './app',
-    vendor: ['react/addons', 'react-router', 'bows', 'fluxxor', 'lodash', 'lunr',
+    vendor: ['react/addons', 'react-router', 'bows', 'fluxxor', 'lodash', 'lodash-node', 'lunr',
      'moment', 'node-uuid', 'superagent', 'tcomb-validation', 'react-textarea-autosize'],
   },
   output: {
@@ -39,7 +39,7 @@ var config = module.exports = {
   },
   plugins: plugins,
   resolve: {
-    extensions: ['', '.js', '.jsx', '.scss'],
+    extensions: ['', '.js', '.jsx', '.scss', '.css'],
   },
   module: {
     loaders: [
@@ -50,14 +50,16 @@ var config = module.exports = {
           'style-loader',
           'css-loader',
           'autoprefixer-loader',
-          'sass-loader?includePaths[]=./app/base/style,includePaths[]=./node_modules'
+          'sass-loader?includePaths[]=./app/base/style,includePaths[]=./node_modules,includePaths[]=./style'
         ],
       },
       { test: /\.css$/,     loader: 'style-loader!css-loader' },
-      { test: /\.png$/,     loader: 'url-loader' },
+      { test: /\.png$/,     loader: 'url-loader?limit=100000&mimetype=image/png'},
+      { test: /\.jpg$/,     loader: 'url-loader?limit=100000&mimetype=image/jpeg' },
       { test: /\.woff$/,    loader: 'url-loader?mimetype=application/font-woff' },
       { test: /\.ttf$/,     loader: 'url-loader?' },
       { test: /\.eot$/,     loader: 'url-loader?' },
+      { test: /\.svg$/,     loader: 'file-loader' },
       { test: /\raw.svg$/,  loader: 'raw-loader?' },
     ],
 },
