534723d648b01d72373e85f057a1f5c8eb457482
deoxxa <deoxxa@fknsrs.biz>
2015-01-21 15:28:41 +1100

    clean up whitespace

--- bin/start.sh
+++ bin/start.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 [ -z "${PORT}" ] && PORT=8080
+
 PATH="./node_modules/macropod-tools/node_modules/.bin:${PATH}"
 
 webpack-dev-server \
-	-d --hot --inline --history-api-fallback --display-reasons \
-	--display-error-details --progress	--colors --port ${PORT} \
-	--output-public-path http://127.0.0.1:${PORT}/
+  -d --hot --inline --history-api-fallback --display-reasons \
+  --display-error-details --progress --colors --port ${PORT} \
+  --output-public-path http://127.0.0.1:${PORT}/
