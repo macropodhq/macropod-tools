c33920b4a97bbcc7fc296c1d0f7e88d50c9a8f03
Nathan Hutchison <nathan@bugherd.com>
2015-01-21 13:48:48 +1100

    another try at finding my self

--- bin/start.sh
+++ bin/start.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 [ -z "${PORT}" ] && PORT=8080
+PATH="$(dirname $(readlink $0))/../node_modules/.bin:${PATH}"
 
-./node_modules/macropod-tools/.bin/webpack-dev-server \
+webpack-dev-server \
 	-d --hot --inline --history-api-fallback --display-reasons \
 	--display-error-details --progress	--colors --port ${PORT} \
 	--output-public-path http://127.0.0.1:${PORT}/
