09740aa7c816cbde225d7186217e9472bf69eb2c
Nathan Hutchison <nathan@bugherd.com>
2015-01-22 14:06:46 +1100

    remove -d from dev script; it overrides config

--- bin/start.sh
+++ bin/start.sh
@@ -5,6 +5,6 @@
 PATH="./node_modules/macropod-tools/node_modules/.bin:${PATH}"
 
 webpack-dev-server \
-  -d --hot --inline --history-api-fallback --display-reasons \
-  --display-error-details --progress --colors --port ${PORT} \
-  --output-public-path http://127.0.0.1:${PORT}/
+--hot --inline --history-api-fallback --display-reasons \
+--display-error-details --progress --colors --port ${PORT} \
+--display-chunks --output-public-path http://127.0.0.1:${PORT}/
