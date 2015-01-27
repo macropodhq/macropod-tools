7bcdf0df3ae710330ff99c9d7f05a771a6bdd4fb
deoxxa <deoxxa@fknsrs.biz>
2015-01-28 10:18:42 +1100

    fix indenting

--- bin/start.sh
+++ bin/start.sh
@@ -5,6 +5,6 @@
 PATH="./node_modules/macropod-tools/node_modules/.bin:${PATH}"
 
 webpack-dev-server \
---hot --inline --history-api-fallback --display-reasons \
---display-error-details --progress --colors --port ${PORT} \
---display-chunks --output-public-path http://127.0.0.1:${PORT}/
+	--hot --inline --history-api-fallback --display-reasons \
+	--display-error-details --progress --colors --port ${PORT} \
+	--display-chunks --output-public-path http://127.0.0.1:${PORT}/
